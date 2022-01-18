9828c69c7cfb95109a2c2c5cfc85228eef8760ff
Praveen Adivi <MadhaviPraveen>
2022-01-17 21:12:00 -0500

    Initial structure and scaffolding

---
+++ src/main/java/com/rbc/stocktickermanager/StocktickermanagerApplication.java
@@ -0,0 +1,13 @@
+package com.rbc.stocktickermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocktickermanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocktickermanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rbc/stocktickermanager/StocktickermanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbc.stocktickermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocktickermanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
